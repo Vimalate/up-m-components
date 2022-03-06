8211c89d0d2f438cc65b6139edc15882373600f2
Vimalate <1902644369@qq.com>
2022-03-06 22:25:06 +0800

    省市区封装

---
+++ src/components/chooseArea/src/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-select placeholder="请选择省份" v-model="province">
+      <el-option>1</el-option>
+    </el-select>
+    <el-select style="margin:0 10px" placeholder="请选择城市" v-model="city">
+      <el-option>1</el-option>
+    </el-select>
+    <el-select placeholder="请选择区域" v-model="area">
+      <el-option>1</el-option>
+    </el-select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+let province = ref<string>('')
+let city = ref<string>('')
+let area = ref<string>('')
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <ChooseArea></ChooseArea>
+</template>
+
+<script lang="ts" setup>
+import ChooseArea from "../../components/chooseArea/src/index.vue"
+</script>
+
+<style lang="scss" scoped>
+</style>

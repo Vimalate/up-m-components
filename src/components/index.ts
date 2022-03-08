de3e28f2c8c87b5b2d572b597cdabfa80eed31d5
Vimalate <1902644369@qq.com>
2022-03-08 21:51:14 +0800

    trend

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,14 @@
 import { App } from "vue";
 import chooseIcon from "./chooseIcon/index";
 import chooseArea from "./chooseArea/index";
+import trend from "./trend/index";
 
-const components = [chooseArea, chooseIcon];
+const components = [chooseArea, chooseIcon, trend];
 
-export default{
+export default {
   install(app: App) {
-    components.map(item=>{
-      app.use(item)
-    })
-    
+    components.map((item) => {
+      app.use(item);
+    });
   },
-}
-
-
+};
---
+++ src/views/trend/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <div class="trend-wrap">
+    <m-trend text="营业额"></m-trend>
+    <m-trend text="销售额" style="margin-left: 20px;" type="down"></m-trend>
+  </div>
+   <div class="trend-wrap">
+    <m-trend text="营业额" upIconColor="orange"></m-trend>
+    <m-trend text="销售额" downIconColor="blue" style="margin-left: 20px;" type="down"></m-trend>
+  </div>-->
+  <div class="trend-wrap">
+    <m-trend upIconColor="orange">营业额</m-trend>
+    <m-trend upIcon="CaretTop">营业额</m-trend>
+  </div>
+
+  <div class="trend-wrap">
+    <m-trend upTextColor="#ccc" text="营业额"></m-trend>
+    <m-trend text="销售额" style="margin-left: 20px;" type="down"></m-trend>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.trend-wrap {
+  display: flex;
+}
+</style>

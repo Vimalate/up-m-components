1b2a2f0673897c28a74a1b6464f27ba90b87447e
Vimalate <1902644369@qq.com>
2022-03-22 20:59:01 +0800

    table可编辑行

--- src/components/table/src/index.vue
+++ src/components/table/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table v-bind="$attrs" v-loading="isLoading" :data="data">
+  <el-table v-bind="$attrs" v-loading="isLoading" :data="tableData" @row-click="rowClick">
     <template v-for="(item, index) in tableOPtions" :key="index">
       <el-table-column
         :width="item.width"
@@ -8,8 +8,13 @@
         :align="item.align"
       >
         <template #default="scope">
-          <slot v-if="item.slot" :name="item.slot" :scope="scope"></slot>
-          <span v-else>{{scope.row[item.prop!]}}</span>
+          <template v-if="scope.row.rowEdit">
+            <el-input v-model="scope.row[item.prop!]"></el-input>
+          </template>
+          <template v-else>
+            <slot v-if="item.slot" :name="item.slot" :scope="scope"></slot>
+            <span v-else>{{ scope.row[item.prop!] }}</span>
+          </template>
         </template>
       </el-table-column>
     </template>
@@ -22,15 +27,17 @@
       :align="actionOptions.align"
     >
       <template #default="scope">
-        <slot name="action" :scope="scope"></slot>
+        <slot v-if="scope.row.rowEdit" name="rowEdit" :scope="scope"></slot>
+        <slot v-else name="action" :scope="scope"></slot>
       </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script setup lang="ts">
-import { PropType, computed } from 'vue';
+import { ref, PropType, computed, onMounted, watch } from 'vue';
 import { TableOptions } from './types';
+import cloneDeep from 'lodash/cloneDeep'
 let props = defineProps({
   options: {
     type: Array as PropType<TableOptions[]>,
@@ -40,11 +47,58 @@ let props = defineProps({
     type: Array as PropType<any[]>,
     required: true
   },
+  isEditRow: {
+    type: Boolean,
+    default: false
+  },
+  editRowIndex: {
+    type: String,
+    default: ''
+  }
 
 })
+
+let emits = defineEmits(['update:editRowIndex'])
+
 const tableOPtions = computed(() => props.options.filter(item => !item.action))
 const actionOptions = computed(() => props.options.find(item => item.action))
 let isLoading = computed(() => !props.data || !props.data.length)
+
+const tableData = ref<any[]>(cloneDeep(props.data))
+let editIndex = ref<string>(props.editRowIndex)
+onMounted(() => {
+  tableData.value.map(item => {
+    item.rowEdit = false
+  })
+})
+watch(() => props.data, val => {
+  tableData.value = cloneDeep(val)
+  tableData.value.map(item => {
+    item.rowEdit = false
+  })
+
+})
+watch(() => props.editRowIndex, val => {
+  val && (editIndex.value = val)
+})
+
+const rowClick = (row: any, column: any) => {
+  if (column.label === actionOptions.value!.label) {
+    console.log('index', column.label);
+    if (props.editRowIndex && props.editRowIndex === editIndex.value) {
+      row.rowEdit = !row.rowEdit
+      tableData.value.map(item => {
+        if (item !== row) {
+          item.rowEdit = false
+        }
+      })
+      if (!row.rowEdit) {
+        emits('update:editRowIndex', '')
+      }
+    }
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,59 +1,71 @@
 <template>
   <div>
-    <m-table element-loading-text="加载中..." :data="tableData" :options="options">
+    <m-table
+      v-model:editRowIndex="editRowIndex"
+      isEditRow
+      element-loading-text="加载中..."
+      :data="tableData"
+      :options="options"
+    >
       <template #date="{ scope }">
         <el-icon-timer></el-icon-timer>
-        {{scope.row.date}}
+        {{ scope.row.date }}
       </template>
       <template #action="{ scope }">
         <el-button type="primary" size="small" @click="edit(scope.row)">编辑</el-button>
         <el-button type="danger" size="small" @click="onDelete(scope.row)">删除</el-button>
+      </template>
+      <template #rowEdit="{ scope }">
+        <el-button type="primary" size="small" @click="edit(scope.row)">确认</el-button>
+        <el-button type="danger" size="small" @click="onDelete(scope.row)">取消</el-button>
       </template>
     </m-table>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { TableOptions } from '../../components/table/src/types';
-interface TableData{
-  name:string,
-  date:string,
-  address:string
+interface TableData {
+  name: string,
+  date: string,
+  address: string
 }
-let tableData=ref<TableData[]>([])
-setTimeout(()=>{
-  tableData.value= [
-  { 
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+let tableData = ref<TableData[]>([])
+setTimeout(() => {
+  tableData.value = [
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-04',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-01',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+  ]
 
-},3000)
+})
 
 const options: TableOptions[] = [
   { label: '日期', slot: 'date', prop: 'date', align: 'center', width: 180 },
   { label: '姓名', prop: 'name', align: 'center' },
   { label: '地址', prop: 'address', align: 'center' },
-  { label: '操作', action: true, align: 'center' },
+  { label: '操作', action: true, align: 'center' },//action 操作栏
 ]
+
+const editRowIndex = ref<string>('')
 
 const onDelete = (row: any) => {
   console.log(row);
@@ -61,11 +73,12 @@ const onDelete = (row: any) => {
 }
 const edit = (row: any) => {
   console.log(row);
+  editRowIndex.value = 'edit'
 }
 </script>
 
 <style lang="scss" scoped>
-svg{
+svg {
   width: 1em;
   height: 1em;
 }

e70e60e37cd8710cb22b50b25fe2a8e7e3e3fc2f
Vimalate <1902644369@qq.com>
2022-03-26 21:55:19 +0800

    添加vitepress组件文档说明

---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+home: true,
+heroText: 基于 element-plus、vue3、vite 二次封装的组件
+tagline: 搞扩展性的组件库
+actionText: 快速开始
+actionLink: //
+features:
+  - title: 简洁至上
+    details: 支持全量引入和按需映入
+  - title: 搞扩展性
+    details: 全新组件api设计，支持高度自定义
+  - title: 全面覆盖
+    details: 涵盖基础组件，通用组件和业务组件
+---
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router/index";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-// import * as Icons from "@element-plus/icons-vue";
-// import { toLine } from "./utils";
+import * as Icons from "@element-plus/icons-vue";
+import { toLine } from "./utils";
 // 全局引入
 import mComponents from "./components/index"
 // import mComponents from "../lib";
@@ -13,10 +13,10 @@ import mComponents from "./components/index"
 // import chooseIcon from "../lib/chooseIcon/index.es";
 // import "../lib/chooseIcon/style.css";
 const app = createApp(App);
-// for (const i in Icons) {
-//   // 注册全局组件
-//   app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
-// }
+for (const i in Icons) {
+  // 注册全局组件
+  app.component(`el-icon-${toLine(i)}`, (Icons as any)[i]);
+}
 app.use(router)
 .use(ElementPlus)
 // .use(chooseIcon);
